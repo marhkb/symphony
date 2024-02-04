563fe7db06346aa0c065ec525573af2041ad088b
Marcus Behrendt <marcus.behrendt.86@gmail.com>
2024-02-04 14:26:25 +0100

    wip

--- src/model/client.rs
+++ src/model/client.rs
@@ -28,6 +28,7 @@ pub(crate) enum ClientError {
     Containers,
     Pods,
     Volumes,
+    Networks,
 }
 
 mod imp {
@@ -52,6 +53,8 @@ mod imp {
         pub(super) pod_list: OnceCell<model::PodList>,
         #[property(get = Self::volume_list)]
         pub(super) volume_list: OnceCell<model::VolumeList>,
+        #[property(get = Self::network_list)]
+        pub(super) network_list: OnceCell<model::NetworkList>,
         #[property(get = Self::action_list)]
         pub(super) action_list: OnceCell<model::ActionList>,
     }
@@ -251,6 +254,12 @@ mod imp {
                 .to_owned()
         }
 
+        fn network_list(&self) -> model::NetworkList {
+            self.network_list
+                .get_or_init(|| model::NetworkList::from(&*self.obj()))
+                .to_owned()
+        }
+
         fn action_list(&self) -> model::ActionList {
             self.action_list
                 .get_or_init(|| model::ActionList::from(&*self.obj()))
@@ -340,6 +349,10 @@ impl Client {
                         let err_op = err_op.clone();
                         |_| err_op(ClientError::Volumes)
                     });
+                    obj.network_list().refresh({
+                        let err_op = err_op.clone();
+                        |_| err_op(ClientError::Networks)
+                    });
 
                     op();
                     obj.start_event_listener(err_op, finish_op);
@@ -369,6 +382,8 @@ impl Client {
                 @weak self as obj => @default-return glib::ControlFlow::Break,
                 move |result: podman::Result<podman::models::Event>|
             {
+                println!("{result:?}");
+
                 match result {
                     Ok(event) => {
                         log::debug!("Event: {event:?}");
@@ -388,6 +403,10 @@ impl Client {
                             "volume" => obj.volume_list().handle_event(event, {
                                 let err_op = err_op.clone();
                                 |_| err_op(ClientError::Volumes)
+                            }),
+                            "network" => obj.network_list().handle_event(event, {
+                                let err_op = err_op.clone();
+                                |_| err_op(ClientError::Networks)
                             }),
                             other => log::warn!("Unhandled event type: {other}"),
                         }
